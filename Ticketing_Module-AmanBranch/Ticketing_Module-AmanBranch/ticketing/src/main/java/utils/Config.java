2e662351e782e66a58dd2e3773227c3b04a2427c
AmanCogniter <108818487+AmanCogniter@users.noreply.github.com>
2022-11-15 10:49:44 +0530

    Delete Ticketing_Module-AmanBranch/Ticketing_Module-AmanBranch/ticketing directory

--- Ticketing_Module-AmanBranch/Ticketing_Module-AmanBranch/ticketing/src/main/java/utils/Config.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-public class Config {
-
-    public static String testfilePDF = System.getProperty("user.dir") + "\\testfiles\\pdf.pdf";
-    public static String testfileDoc = System.getProperty("user.dir") + "\\testfiles\\file-sample_100kB.doc";
-
-}
